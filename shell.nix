a5dfde8be70653fc9fafb991df90f039f2f329c0
Peter Jones <pjones@devalot.com>
2019-06-28 17:36:40 -0700

    Add generated frequency file

---
+++ shell.nix
@@ -0,0 +1,21 @@
+{ pkgs ? (import <nixpkgs> {}).pkgs
+}:
+
+let
+  nix-hs-src = fetchGit {
+    url = "https://code.devalot.com/pjones/nix-hs.git";
+    rev = "2cc803c62cdc74a9a0696cc8fa8ffa3fcb420b97";
+  };
+
+  nix-hs = (import "${nix-hs-src}/default.nix" {inherit pkgs;});
+
+in
+
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    nix-hs
+    haskellPackages.hlint
+    haskellPackages.hasktags
+    # cabal-dependency-licenses
+  ];
+}
--- zxcvbn-hs.nix
+++ zxcvbn-hs.nix
@@ -1,8 +1,11 @@
-{ mkDerivation, base, stdenv }:
+{ mkDerivation, base, cereal, cereal-text, containers, stdenv, text
+}:
 mkDerivation {
   pname = "zxcvbn-hs";
   version = "0.1.0.0";
   src = ./.;
-  libraryHaskellDepends = [ base ];
+  libraryHaskellDepends = [
+    base cereal cereal-text containers text
+  ];
   license = stdenv.lib.licenses.mit;
 }
