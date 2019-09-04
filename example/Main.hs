a1a1800fa2b4af89aa8c19f5346f15eb517362ed
Peter Jones <pjones@devalot.com>
2019-09-04 14:25:35 -0700

    Add an example

---
+++ example/Main.hs
@@ -0,0 +1,42 @@
+{-|
+
+Copyright:
+  This file is part of the package zxcvbn-hs. It is subject to the
+  license terms in the LICENSE file found in the top-level directory
+  of this distribution and at:
+
+    https://code.devalot.com/open/zxcvbn-hs
+
+  No part of this package, including this file, may be copied,
+  modified, propagated, or distributed except according to the terms
+  contained in the LICENSE file.
+
+License: MIT
+
+-}
+module Main (main) where
+
+--------------------------------------------------------------------------------
+-- Imports:
+import qualified Data.Text.IO as Text
+import Data.Time.Clock (getCurrentTime, utctDay)
+import System.IO
+import Text.Password.Strength
+
+--------------------------------------------------------------------------------
+-- | Read a password from stdin and score it.
+main :: IO ()
+main = do
+  -- Turn off buffering for the password prompt below:
+  hSetBuffering stdout NoBuffering
+
+  -- Get the date and read a password:
+  refDay   <- utctDay <$> getCurrentTime
+  password <- putStr "Password: " >> Text.getLine
+
+  -- Get the score for the password we just read:
+  let s = score en_US refDay password
+
+  -- Output some info about the score:
+  putStrLn ("   Score: " ++ show (getScore s))
+  putStrLn ("Strength: " ++ show (strength s))
--- src/Text/Password/Strength/Internal/Dictionary.hs
+++ src/Text/Password/Strength/Internal/Dictionary.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE TupleSections   #-}
-
 {-|
 
 Copyright:
