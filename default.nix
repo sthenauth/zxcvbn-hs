cfd4913739ef1d84011258928749497a6b4d4080
Peter Jones <pjones@devalot.com>
2020-04-24 16:20:25 -0700

    Build with GHC 8.6.5 and 8.8.3 under NixOS 20.03

--- default.nix
+++ default.nix
@@ -1,30 +1,24 @@
-{ pkgs ? import <nixpkgs> { }
-, useLatestPackages ? false
+{ sources ? import ./nix/sources.nix
+, pkgs ? import sources.nixpkgs { }
+, nix-hs ? import sources.nix-hs { inherit pkgs; }
+, ghcide ? sources.ghcide-nix
+, ormolu ? sources.ormolu
+, ghc ? "default"
 }:
 
-let
-  nix-hs-src = fetchGit {
-    url = "https://code.devalot.com/open/nix-hs.git";
-    rev = "2003332a1e8e518b54e6143f9a9467a8a05abca4";
-  };
-
-  nix-hs = import "${nix-hs-src}/default.nix" { inherit pkgs; };
-
-in nix-hs {
+nix-hs {
   cabal = ./zxcvbn-hs.cabal;
   flags = [ "tools" ];
+  compiler = ghc;
 
   overrides = lib: self: super: with lib; {
     pipes-text = unBreak (dontCheck (doJailbreak super.pipes-text));
 
-    optparse-applicative =
-      if useLatestPackages && super ? optparse-applicative_0_15_0_0
-        then super.optparse-applicative_0_15_0_0
-        else super.optparse-applicative;
+    ghcide = import ghcide {};
 
-    lens =
-      if useLatestPackages && super ? lens_4_18
-        then super.lens_4_18
-        else super.lens;
+    ormolu = (import ormolu {
+      inherit (lib) pkgs;
+      ormoluCompiler = lib.compilerName;
+    }).ormolu;
   };
 }
